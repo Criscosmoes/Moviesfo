ba1076fa5261d3770a4d7928c22a0f638b5a1730
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-04-05 23:09:36 -0700

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
-//
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+// components
+
+import NavBar from "../src/components/NavBar/NavBar";
 
 export default function Home() {
+  console.log(styles.main);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,25 +17,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <div className={styles.content}>
+        <NavBar />
+      </div>
     </div>
   );
 }
