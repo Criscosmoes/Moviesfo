ba37ec645bd924772a6eb53f1c2e63638de30a2e
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-04-05 23:37:06 -0700

    Revert "added navbar"
    This reverts commit ba1076fa5261d3770a4d7928c22a0f638b5a1730.

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
-// components
-
-import NavBar from "../src/components/NavBar/NavBar";
+//
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Home() {
-  console.log(styles.main);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -17,9 +21,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.content}>
-        <NavBar />
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
   );
 }
